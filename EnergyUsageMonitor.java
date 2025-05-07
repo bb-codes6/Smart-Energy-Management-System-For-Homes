a418bb175f9e0bf60b353c9e14a49f137757f021
Brittany Caldwell <britcaldwell@yahoo.com>
2025-05-07 11:10:47 -0500

    Create EnergyUsageMonitor.java

---
+++ EnergyUsageMonitor.java
@@ -0,0 +1,11 @@
+public class EnergyUsageMonitor {
+  private List<SmartAppliance> appliances;
+
+public double getCurrentEnergyConsumption() {
+  // Sum energy from all appliances
+}
+
+public void trackEnergyUsage() {
+  // Store and analyze consumption over time
+ }
+}
